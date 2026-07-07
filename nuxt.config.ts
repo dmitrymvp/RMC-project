605e484f88263734608700143b0872b8caa99a16
DmitryMVP <masqueunclub17@gmail.com>
2026-07-07 10:58:50 +0500

    feat(build): подключить глобальные Sass mixins/tokens и базовый reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,19 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
+  css: ['~/assets/styles/reset.sass'],
   eslint: {
     config: {
       stylistic: false
+    }
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: '@use "@/assets/mixins" as mixins\n@use "@/assets/tokens/colors" as tokens\n'
+        }
+      }
     }
   }
 })
