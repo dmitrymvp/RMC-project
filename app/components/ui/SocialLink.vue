cbb1e2565cddf4ea282be36f54d04479d2363d0f
DmitryMVP <masqueunclub17@gmail.com>
2026-07-07 19:03:04 +0500

    feat(ui): добавить компонент SocialLink

---
+++ app/components/ui/SocialLink.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import type { RouteLocationRaw } from 'vue-router'
+
+type SocialNetwork = 'vk' | 'telegram'
+
+interface Props {
+  network: SocialNetwork
+  to: RouteLocationRaw
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <NuxtLink :to="to" class="social-link" target="_blank" rel="noopener noreferrer">
+    <SvgoSocialVk v-if="network === 'vk'" />
+    <SvgoSocialTg v-else />
+  </NuxtLink>
+</template>
+
+<style lang="sass" scoped>
+.social-link
+  display: flex
+  justify-content: center
+  align-items: center
+  width: 128px
+  height: 128px
+  border-radius: 5px
+  background-color: $dark-gray
+  color: $black
+  @include transition
+
+  :deep(.nuxt-icon)
+    width: 45px
+    height: auto
+    margin-bottom: 0
+
+  @include hover
+    background-color: $blue
+    color: $very-light-grey
+
+  @include laptop
+    width: 80px
+    height: 80px
+    :deep(.nuxt-icon)
+      width: 40px
+
+  @include tablet
+    width: 40px
+    height: 40px
+    :deep(.nuxt-icon)
+      width: 30px
+</style>
