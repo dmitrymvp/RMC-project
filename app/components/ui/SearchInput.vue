11bade07ab8b7dee0756614087e292351eeefde9
DmitryMVP <masqueunclub17@gmail.com>
2026-07-09 13:46:52 +0500

    feat(ui): добавить компонент SearchInput

---
+++ app/components/ui/SearchInput.vue
@@ -0,0 +1,183 @@
+<script setup lang="ts">
+import { ref, useTemplateRef } from 'vue'
+
+interface Props {
+  surface?: 'dark' | 'light'
+  placeholder?: string
+  buttonText?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  surface: 'dark',
+  placeholder: 'Поиск',
+  buttonText: 'Найти'
+})
+
+const value = defineModel<string>('value', { default: '' })
+
+const isFocused = ref(false)
+
+const inputRef = useTemplateRef('input')
+
+function handleClear() {
+  value.value = ''
+  inputRef.value?.focus()
+}
+</script>
+
+<template>
+  <form
+    class="search-input"
+    :class="{ 'search-input_surface-light': surface === 'light', 'search-input_active': isFocused }"
+  >
+    <div class="search-input__wrapper-field">
+      <input
+        ref="input"
+        v-model="value"
+        class="search-input__field"
+        type="text"
+        :placeholder="placeholder"
+        @focus="isFocused = true"
+        @blur="isFocused = false"
+      />
+      <button
+        v-show="value.length > 0"
+        type="button"
+        class="search-input__clear"
+        @click="handleClear"
+      ></button>
+    </div>
+    <div class="wrapper-button">
+      <UiButton
+        type="submit"
+        :text="buttonText"
+        without-arrow
+        :modifier="surface === 'light' ? 'button_blue' : 'button_light-grey'"
+      />
+    </div>
+  </form>
+</template>
+
+<style lang="sass" scoped>
+.search-input
+  position: relative
+  display: flex
+  justify-content: space-between
+  align-items: center
+  width: 100%
+  height: 75px
+  border: 3px solid $light-grey
+  border-radius: 100px
+  box-shadow: 0 0 25px rgba(113, 110, 219, 0.15)
+  background-color: transparent
+
+  @include transition
+
+  @include hover
+    border-color: $very-light-grey
+
+  @include tablet
+    height: 55px
+
+  &__wrapper-field
+    display: flex
+    justify-content: space-between
+    flex-grow: 1
+    padding: 0 0px 0 30px
+
+  &__field
+    border: none
+    background: transparent
+    flex-grow: 1
+    padding-right: 30px
+    color: $light-grey
+    @include text-data-teg
+
+
+    &::placeholder
+      color: $light-grey
+
+    &:focus
+      outline: none
+
+
+  &__clear
+    width: 22px
+    height: 22px
+    color: $white
+    margin-right: 5px
+    &::before,
+    &::after
+      display: block
+      content: ''
+      width: 20px
+      height: 3px
+      border-radius: 100px
+      background-color: currentColor
+      position: absolute
+    &::before
+      transform:  rotate(45deg)
+
+    &::after
+      transform:  rotate(-45deg)
+
+
+    &_active
+      border-color: $very-light-grey
+      background-color: $light-grey
+
+
+  .wrapper-button
+    position: relative
+    left: 3px
+
+    :deep(.button_blue:hover)
+      background-color: transparent
+      color: $blue
+
+    :deep(.button_light-grey:hover)
+      background-color: $very-light-grey
+      border-color: $very-light-grey
+      color: $black
+
+
+  &.search-input_active
+    background-color: $light-grey
+    border-color: $very-light-grey
+
+    .search-input__field
+      color: $dark-gray
+
+      &::placeholder
+        color: $dark-gray
+
+  &_surface-light
+    border-color: $gray
+
+    .search-input__field
+      color: $gray
+      &::placeholder
+        color: $gray
+
+    .search-input__clear
+      color: $blue
+
+    &:not(.search-input_active)
+      @include hover
+        border-color: $blue
+
+        .search-input__field
+          color: $blue
+          &::placeholder
+            color: $blue
+
+    &.search-input_active
+      background-color: transparent
+      border-color: $blue
+
+      .search-input__field
+        color: $dark-gray
+
+        &::placeholder
+          color: $dark-gray
+</style>
